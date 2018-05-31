Fodoj/stronger_parameters

Implement `NullStringConstraint`
Useful to validate against nil, '', 'undefined'

lib/stronger_parameters/constraints.rb
@@ -82,3 +82,4 @@ require 'stronger_parameters/constraints/array_constraint'
 require 'stronger_parameters/constraints/hash_constraint'
 require 'stronger_parameters/constraints/enumeration_constraint'
 require 'stronger_parameters/constraints/comparison_constraints'
+require 'stronger_parameters/constraints/null_string_contraint'

Wolox/strong-parameters

lib/stronger_parameters/constraints.rb
@@ -85,3 +85,4 @@ require 'stronger_parameters/constraints/array_constraint'
 require 'stronger_parameters/constraints/hash_constraint'
 require 'stronger_parameters/constraints/enumeration_constraint'
 require 'stronger_parameters/constraints/comparison_constraints'
+require 'stronger_parameters/constraints/nil_string_contraint'